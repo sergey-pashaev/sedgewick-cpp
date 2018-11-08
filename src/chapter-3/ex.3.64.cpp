dca9c5c9467b8a9474b57e91ec4f4f5d37eeeabd
Sergey Pashaev <sergey.pashaev@gmail.com>
2018-11-08 10:02:05 +0400

    Add ex.3.64

---
+++ src/chapter-3/ex.3.64.cpp
@@ -0,0 +1,30 @@
+// ex.3.64
+
+// en: Write a program to fill in a two-dimensional array of 0 - 1
+// values by setting a[i][j] to 1 if the greatest common divisor of i
+// and j is 1, and to 0 otherwise.
+
+// ru: Напишите программу заполнения двумерного массива значениями 0 и
+// 1: элемент a[i][j] должен содержать 1, если наибольший общий
+// делитель i и j равен единице, и значение 0 в остальных случаях.
+
+#include <array>
+#include <iostream>
+
+using std::array;
+
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+
+int main() {
+    const int N = 20;
+    const int M = 20;
+    array<array<int, M>, N> arr = {};
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            arr[i][j] = gcd(i, j) == 1;
+            std::cout << arr[i][j] << (j + 1 < N ? " " : "");
+        }
+        std::cout << '\n';
+    }
+}
