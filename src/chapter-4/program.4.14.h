37a83efc3505101577f443d824cfb63572e4119e
Sergey Pashaev <sergey.pashaev@gmail.com>
2018-11-10 12:29:35 +0400

    Add program.4.14

---
+++ src/chapter-4/program.4.14.h
@@ -0,0 +1,50 @@
+#ifndef PROGRAM_4_14_H
+#define PROGRAM_4_14_H
+
+// program.4.14
+
+// en: FIFO queue linked-list implementation
+
+// ru: Реализация очереди FIFO на базе связного списка
+
+template <typename T>
+class Queue {
+   private:
+    struct node {
+        node(T v) : item{v}, next{nullptr} {}
+
+        T item;
+        node* next;
+    };
+    typedef node* link;
+    link head_;
+    link tail_;
+
+   public:
+    Queue(int) { head_ = nullptr; }
+
+    bool empty() const { return head_ == nullptr; }
+
+    void put(T x) {
+        link t = tail_;
+        tail_ = new node(x);
+
+        if (head_) {
+            t->next = tail_;
+        } else {
+            head_ = tail_;
+        }
+    }
+
+    T get() {
+        T v = head_->item;
+        link t = head_->next;
+        delete head_;
+        head_ = t;
+        return v;
+    }
+
+   private:
+};
+
+#endif /* PROGRAM_4_14_H */
