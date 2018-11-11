598ff98f8e2e23585e520449473a4851faa4c155
Sergey Pashaev <sergey.pashaev@gmail.com>
2018-11-11 12:19:41 +0400

    Add extra directory w/ target for final coveralls upload
    - extract KCOV_ARGS, KCOV_REPORT_PATH vars
    - accumulate all coverage data in one place & make one upload

--- test/coverage.sh
+++ test/coverage.sh
@@ -8,13 +8,12 @@ fi
 
 curdir=`pwd -P`
 prg="$1"
-travis=$2
-input=$3
-args=${@:4}
+input=$2
+args=${@:3}
 output="${input%.in}.out"
 
 tmpdir=`mktemp -d $prg.tmp.XXXXXX`
 trap "rm -r $tmpdir" EXIT
 
-(cd $tmpdir && kcov --coveralls-id="$travis" --exclude-pattern=/usr/include,thirdparty "${curdir}/report/" $prg $args < $input > out)
+(cd $tmpdir && kcov --exclude-pattern=/usr/include,thirdparty /tmp/kcov $prg $args < $input > out)
 diff -u $tmpdir/out $output
