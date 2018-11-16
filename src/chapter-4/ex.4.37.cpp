a47a7a8b8c1c0a9de6429c8eedcd815e625086b1
Sergey Pashaev <sergey.pashaev@gmail.com>
2018-11-16 08:43:32 +0400

    Add ex.4.37

---
+++ src/chapter-4/ex.4.37.cpp
@@ -0,0 +1,33 @@
+// ex.4.36
+
+// en: A letter means put and an asterisk means get in the sequence
+//   E A S * Y * Q U E * * * S T * * * I O * N * * *
+// Give the sequence of values returned by the get operations when
+// this sequence of operations is performed on an initially empty FIFO
+// queue.
+
+// ru: В последовательности
+//   E A S * Y * Q U E * * * S T * * * I O * N * * *
+// буква означает операцию занести, а звездочка - операцию
+// извлечь. Найдите последовательность значений, возвращаемых
+// операциями извлечь, если эта последовательность операций
+// выполняется над первоначально пустой очередью FIFO.
+
+#include <iostream>
+
+#include "program.4.15.h"
+
+int main() {
+    Queue<char> q{10};
+    const char* input = "EAS*Y*QUE***ST***IO*N***";
+    while (*input) {
+        if (*input == '*')
+            std::cout << q.get() << '\n';
+        else
+            q.put(*input);
+
+        ++input;
+    }
+
+    return 0;
+}
---
+++ src/chapter-4/ex.4.37.md
@@ -0,0 +1,39 @@
+# ex.4.37
+
+## task
+
+en: A letter means put and an asterisk means get in the sequence
+
+    E A S * Y * Q U E * * * S T * * * I O * N * * *
+
+Give the sequence of values returned by the get operations when this
+sequence of operations is performed on an initially empty FIFO queue.
+
+ru: В последовательности
+
+    E A S * Y * Q U E * * * S T * * * I O * N * * *
+
+буква означает операцию занести, а звездочка - операцию
+извлечь. Найдите последовательность значений, возвращаемых операциями
+извлечь, если эта последовательность операций выполняется над
+первоначально пустой очередью FIFO.
+
+## solution
+
+[ex.4.37.cpp](./ex.4.37.cpp)
+
+```
+$ ./ex.4.37
+E
+A
+S
+Y
+Q
+U
+E
+S
+T
+I
+O
+N
+```
