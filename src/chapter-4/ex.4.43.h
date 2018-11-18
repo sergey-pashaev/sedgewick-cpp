09e82b4daae34e590907236cce7dfe2891e4b4c9
Sergey Pashaev <sergey.pashaev@gmail.com>
2018-11-18 15:14:29 +0400

    Add ex.4.43

---
+++ src/chapter-4/ex.4.43.h
@@ -0,0 +1,21 @@
+#ifndef EX_4_43_H
+#define EX_4_43_H
+
+// ex.4.43
+
+// en: Write an interface for the deque ADT.
+
+// ru: Напишите интерфейс для АТД дека.
+
+template <typename T>
+class Deque {
+   public:
+    Deque(int);
+    bool Empty() const;
+    void PushFront(T v);
+    void PushBack(T v);
+    T PopFront();
+    T PopBack();
+};
+
+#endif /* EX_4_43_H */
