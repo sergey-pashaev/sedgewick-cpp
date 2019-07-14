fbe5eb68583c45a2db18682deab1ef199e16292a
Sergey Pashaev <sergey.pashaev@gmail.com>
2019-07-14 11:45:33 +0400

    ex.5.2: use modulo properties to fix calculations on large m

--- src/chapter-5/ex.5.2.cpp
+++ src/chapter-5/ex.5.2.cpp
@@ -11,35 +11,46 @@
 // система программирования обрабатывает рекурсивные вызовы с большой
 // глубиной вложенности.
 
-#include <cassert>
-#include <cmath>
-#include <cstdint>
-#include <iostream>
-
 #include <catch.hpp>
 
-uint64_t mod_factorial(uint64_t n, uint64_t m) {
-    if (!n) return 1;
-    return (mod_factorial(n - 1, m) * n) % m;
+// n! % m = (n * n-1 * ... * 2 * 1) % m
+
+// (n * n-1 * ... * 2 * 1) % m =
+// = ((n % m) * (n-1 % m) * ... * (2 % m) * (1 % m)) % m
+
+// if n >= m : somewhere in this product n % m = 0 so whole product will be 0
+
+// if n < m : n % m = n, so product = (n * n - 1 * ... 2 * 1) % m , so
+// in order to avoid overflow we might calculate % m on each step
+
+int mod_factorial_i(int n, int m) {
+    if (n >= m) return 0;
+    int ret = 1;
+    for (int i = 1; i <= n; ++i) ret = (ret * i) % m;
+    return ret;
 }
 
-int usage(const char* bin) {
-    std::cout << "Usage: " << bin << " <positive int N> <positive int M>\n";
-    return 1;
+int mod_factorial_r(int n, int m) {
+    if (n == 1) return n % m;
+    return ((n % m) * mod_factorial_r(n - 1, m)) % m;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        return usage(argv[0]);
-    }
+TEST_CASE("modulo of factorial: iterative") {
+    REQUIRE(mod_factorial_i(5, 7) == 1);
+    REQUIRE(mod_factorial_i(6, 7) == 6);
 
-    int n = std::atoi(argv[1]);
-    int m = std::atoi(argv[2]);
-    if (n <= 0 || m <= 0) {
-        return usage(argv[0]);
-    }
+    REQUIRE(mod_factorial_i(997, 1000) == 0);
+    REQUIRE(mod_factorial_i(997, 10'000) == 0);
+    REQUIRE(mod_factorial_i(997, 100'000) == 0);
+    REQUIRE(mod_factorial_i(997, 1'000'000) == 0);
+}
 
-    std::cout << mod_factorial(n, m) << '\n';
+TEST_CASE("modulo of factorial: recursive") {
+    REQUIRE(mod_factorial_r(5, 7) == 1);
+    REQUIRE(mod_factorial_r(6, 7) == 6);
 
-    return 0;
+    REQUIRE(mod_factorial_r(997, 1000) == 0);
+    REQUIRE(mod_factorial_r(997, 10'000) == 0);
+    REQUIRE(mod_factorial_r(997, 100'000) == 0);
+    REQUIRE(mod_factorial_r(997, 1'000'000) == 0);
 }
